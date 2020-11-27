1e20bf28ba2f1f69ed6be40eb4bcd176edb1e69a
Ole Martin Bjørndalen <ombdalen@gmail.com>
2020-11-27 03:44:28 +0100

    Improved code.

--- minesweeper.py
+++ minesweeper.py
@@ -13,17 +13,12 @@ class Tile:
 
 
 class Minesweeper:
-    def __init__(self, size, num_bombs, show_func, boom_func):
+    def __init__(self, size, num_bombs):
         self.size = size
         self.num_bombs = num_bombs
 
         self.tiles = {(x, y): Tile(x, y) for x in range(self.size) for y in range(self.size)}
         self._place_bombs()
-
-        self.show_func = show_func
-        self.boom_func = boom_func
-
-        self.boom = False
 
     def _iter_neighbours(self, tile):
         (x, y) = (tile.x, tile.y)
@@ -42,23 +37,17 @@ class Minesweeper:
                 neighbour.count += 1
 
     def _uncover(self, tile):
-        if not tile.is_covered or tile.is_bomb:
-            return
-
         tile.is_covered = False
-        self.show_func(tile.x, tile.y)
+        yield tile
 
         if tile.count == 0:
             for neighbour in self._iter_neighbours(tile):
-                self._uncover(neighbour)
+                if neighbour.is_covered and not neighbour.is_bomb:
+                    yield from self._uncover(neighbour)
 
     def step(self, x, y):
-        tile = self.tiles[(x, y)]
-
-        self._uncover(tile)
-        if tile.is_bomb:
-            self.boom = True
-            self.boom_func()
+        if self.tiles[(x, y)].is_covered:
+            yield from self._uncover(self.tiles[(x, y)])
 
 
 class GUI:
@@ -94,30 +83,28 @@ class GUI:
         self.new_game(10, 10)
 
     def new_game(self, size, num_bombs):
-        self.tiles = {}
+        self.covers = {}
         self.flags = {}
 
         self.size = size
         self.num_bombs = num_bombs
 
-        self.canvas.delete('tiles')
+        self.canvas.delete('covers')
         self.canvas.delete('board')
 
         sz = self.tile_size
 
         self.canvas.config(width=size*sz, height=size*sz)
-        self.game = Minesweeper(self.size, self.num_bombs,
-                                show_func=self.remove_tile,
-                                boom_func=self.boom)
+        self.game = Minesweeper(self.size, self.num_bombs)
 
         self.create_board()
 
         rect = self.canvas.create_rectangle
         for (x, y) in self.game.tiles:
-            tile = rect(x*sz, y*sz, (x+1)*sz, (y+1)*sz,
-                        fill='gray60', tag='tiles')
-
-            self.tiles[(x, y)] = tile
+            cover = rect(x*sz, y*sz, (x+1)*sz, (y+1)*sz,
+                         fill='gray60', tag='covers')
+            
+            self.covers[(x, y)] = cover
 
     def tile_xy(self, event):
         return (event.x // self.tile_size, event.y // self.tile_size)
@@ -126,24 +113,27 @@ class GUI:
         x, y = self.tile_xy(event)
 
         if (x, y) not in self.flags:
-            self.game.step(x, y)
+            result = self.game.step(x, y)
+            for tile in self.game.step(x, y):
+                self.canvas.delete(self.covers.pop((tile.x, tile.y)))
+
+                if tile.is_bomb:
+                    # Game over!
+                    self.canvas.itemconfigure('covers', stipple='gray25')
 
     def toggle_flag(self, event):
         x, y = self.tile_xy(event)
 
         try:
-            flag = self.flags[(x, y)]
-            del self.flags[(x, y)]
-            self.canvas.delete(flag)
+            self.canvas.delete(self.flags.pop((x, y)))
         except KeyError:
-            if (x, y) in self.tiles:
+            if self.game.tiles[(x, y)].is_covered:
                 sz = self.tile_size
                 rect = self.canvas.create_rectangle
-                flag = rect((x+0.2)*sz, (y+0.1)*sz,
-                            (x+0.8)*sz, (y+0.5)*sz,
-                            tag='board',
-                            fill='white')
-                self.flags[x, y] = flag
+                self.flags[x, y] = rect((x+0.2)*sz, (y+0.1)*sz,
+                                        (x+0.8)*sz, (y+0.5)*sz,
+                                        tag='board',
+                                        fill='white')
 
     def create_board(self):
         sz = self.tile_size
@@ -160,13 +150,6 @@ class GUI:
                                         text=repr(tile.count),
                                         anchor='center',
                                         tag='board')
-
-    def boom(self):
-        self.canvas.itemconfigure('tiles', stipple='gray25')
-
-    def remove_tile(self, x, y):
-        self.canvas.delete(self.tiles[x, y])
-        del self.tiles[x, y]
 
     def mainloop(self):
         self.tk.mainloop()
